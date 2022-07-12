bf7174dd5cacc5dd83e21d742ffbe09cfe70fac1
Peteris <blankfeldspeteris@gmail.com>
2022-07-12 11:26:04 +0300

    Made function that creates 16 x 16 grid of square divs
    To create 16 x 16 I used css grid, it only requires 2 parameters in order to create a grid row and column.
    
    Function that creates grid, function grid(row,column). It creates rows and columns and then fills it up with square divs.
    
    *
    The way css properties are synchronized with javascript is by root: in css, it is less of a mess than to use DOM manipulation in javascript.

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+
+    
+function grid(row,column) {
+
+    document.documentElement.style.setProperty(`--rowNum`, row);
+    document.documentElement.style.setProperty(`--colNum`, column);
+
+    //This variable calculates square size so it is equally distributed across the .container
+    let squareSize = Math.round(((container.clientHeight * container.clientWidth) / (row * column)) /4);  
+
+
+    for(let i = 0; i < (row * column); i++) {
+        const div = document.createElement('div');
+            div.classList.add('grid-item');
+            div.style.height = squareSize;
+            div.style.width = squareSize;
+
+container.appendChild(div);
+}
+
+}
+
+grid(16,16);
