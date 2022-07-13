0dc31204b0680037b4a2fb580b533627c19ceed7
Peteris <blankfeldspeteris@gmail.com>
2022-07-13 16:01:05 +0300

    Updated hover effect
    When the mouse passes over the grid it changes color to random color.
    
    Random color is generated via randomRgb function.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const container = document.querySelector('.container');
 const square = document.querySelectorAll('.grid-item');
+const button = document.querySelector('button');
 
+
+function randomRgb() { //Generates random rgb value
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+        return `rgb(${x},${y},${z})`;
+ }  
+ 
     
 function grid(row,column) {
 
@@ -8,22 +17,31 @@ function grid(row,column) {
     document.documentElement.style.setProperty(`--colNum`, column);
 
     //This variable calculates square size so it is equally distributed across the .container
-    let squareSize = Math.round(((container.clientHeight * container.clientWidth) / (row * column)) /4);  
-
+    let squareSize = Math.round(((container.clientHeight * container.clientWidth) / (row * column)) /4); 
+    
 
     for(let i = 0; i < (row * column); i++) {
         const div = document.createElement('div');
             div.classList.add('grid-item');
             div.style.height = squareSize;
             div.style.width = squareSize;
-                div.addEventListener('mouseover',changeColor = () => {
-                div.style.backgroundColor = 'black';
-                    
-            });
 
+                div.addEventListener('mouseover',changeColor = () => {
+                    div.style.backgroundColor = randomRgb();// Generate random color         
+            });
 container.appendChild(div);
 }
 }
 
 grid(16,16);
 
+
+    button.addEventListener('click', changeGrid = () => {
+        let input = prompt('Your input');
+            if(input <= 100){
+                container.innerHTML = '';// Deletes current grid, so you can put new grid
+                    grid(input,input);
+            } else {
+                alert('Maximum size of grid is 100 x 100');
+            }   
+    })
